a0fa65ee12a2775c383225a777155ab7d9b72937
Sergey Galkin <sgalkin@users.noreply.github.com>
2022-10-30 22:54:33 +0100

    add light broadcast

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+function toggleLight () {
+    if (light2 == 0) {
+        light2 = 1
+    } else {
+        light2 = 0
+    }
+}
 input.onButtonPressed(Button.A, function () {
     velocity = Math.min(velocity + 10, 90)
     radio.sendNumber(velocity)
@@ -9,6 +16,12 @@ input.onButtonPressed(Button.B, function () {
 input.onGesture(Gesture.Shake, function () {
     radio.sendString("halt")
 })
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    toggleLight()
+    radio.sendValue("light", light2)
+})
+let light2 = 0
 let velocity = 0
 radio.setGroup(42)
 velocity = 0
+light2 = 0
